c3f3c398e165253249a61522945d4f5e40f5b90f
Hannah Angle <hannahcangle@gmail.com>
2020-08-25 22:13:34 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,5 @@
 INSERT INTO bears VALUES (1, "Mr. Chocolate", 3, "M", "black", "active", 1);
-INSERT INTO bears VALUES (2, "Rowdy", 3. "F", "black", "sleepy", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 3, "F", "black", "sleepy", 1);
 INSERT INTO bears VALUES (3, "Tabitha", 8, "F", "brown", "loner", 1);
 INSERT INTO bears VALUES (4, "Sergeant Brown", 13, "M", "brown", "playful", 1);
 INSERT INTO bears VALUES (5, "Melissa", 2, "F", "brown", "quiet", 1);
